42416a58d0e47e1fd1bb55a650233f4f54a1d359
AnthonyAkiniz <70271226+AnthonyAkiniz@users.noreply.github.com>
2022-05-29 00:45:50 -0500

    Add files via upload

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+function About() {
+  return (
+    <>
+      <h1 className='text-6xl mb-4'>GitHub Search Engine</h1>
+      <p className='mb-4 text-2xl font-light'>
+      Search GitHub by name or company and the app connects to the GitHub API to display the data. 
+      Created with React, Tailwind CSS and Daisy UI.
+      </p>
+    </>
+  )
+}
+
+export default About
